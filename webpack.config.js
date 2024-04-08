8c9c4cf508378dac8f062a680c633b27ad3bd9cf
Stephen <stephen@behnfeldt.pro>
2024-04-08 08:26:32 -0400

    Add webpack dev server.

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
+    devServer: {
+        static: './dist',
+    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
